77531a4b7b5722cf4ee6ab0355abcc812efd4ad0
Ylyas <86777995+piraspberr@users.noreply.github.com>
2022-09-17 16:11:42 +0500

    Update and rename app.js to server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var io = require("socket.io")(http);
 
 var port = process.env.port || 3000;
 
-app.use(express.static(__dirname + "/templates" ));
+app.use(express.static(__dirname + "/public" ));
 
 app.get('/',function(req,res){
 res.redirect('index.html');
